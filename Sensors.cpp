756d4b788b79b756f4bf717aedf44970c7108dd3
Chris Hajduk <chrishajduk84@hotmail.com>
2017-10-30 19:33:15 -0400

    Sensor class fixed compilation errors.

--- Sensors.cpp
+++ Sensors.cpp
@@ -1,9 +1,7 @@
 #include "Sensors.h"
-#include <Adafruit_MAX31855.h>
 #include <SPI.h>
-#include <math.h>
 
-Sensors::Sensors(int sensorIndex){
+Sensors::Sensors(int sensorIndex):thermocouple(*pTherm){
 	pP_Abs = (P_AbsPinout + sensorIndex);
 	pP_Gauge = (P_GaugePinout + sensorIndex);
 	pTherm = (ThermPinout + sensorIndex);
@@ -12,46 +10,53 @@ Sensors::Sensors(int sensorIndex){
 	pO2 = &O2Pinout;
 	pO2Therm = &O2ThermPinout;
 	pHeaterCurrent = (HeaterCurrentPinout + sensorIndex);
-	Adafruit_MAX31855 thermocouple(*pTherm);
 }
 
-void Sensors::getP_Abs(){
-	valP_Abs = (analogRead(*pP_abs)-91.80193)/27.3;
+float Sensors::getP_Abs(){
+	valP_Abs = (analogRead(*pP_Abs)-91.80193)/27.3;
+  return valP_Abs;
 }
 
-void Sensors::getP_Gauge(){
+float Sensors::getP_Gauge(){
 	valP_Gauge = ((analogRead(*pP_Gauge)/1023.0)-0.04)/(0.009/0.145038);
+  return valP_Gauge;
 }
 
-void Sensors::getTherm(){
+float Sensors::getTherm(){
 	valTherm = thermocouple.readCelsius();
+  return valTherm;
 }
 
-void Sensors::getFlow(){
+float Sensors::getFlow(){
 	valFlow = (analogRead(*pFlow)-102)/410.0;
+  return valFlow;
 }
 
-void Sensors::getTFlow(){
+float Sensors::getTFlow(){
 	valTFlow = ((analogRead(*pTFlow)/204.6)-1.2)/0.96;
+  return valTFlow;
 }
 
-void Sensors::getO2(){
+float Sensors::getO2(){
 	valO2 = (analogRead(*pO2)/(204.6*38.45*0.000585));
+  return valO2;
 }
 
-void Sensors::getO2Therm(){
-	THERM_RESIST = 24900*((V3_3/(analogRead(*pO2Therm)/204.6))-1);
+float Sensors::getO2Therm(){
+	//THERM_RESIST = 24900*((V3_3/(analogRead(*pO2Therm)/204.6))-1);
 	valO2Therm = (1.0/(THERM_A + THERM_B*log(THERM_RESIST) + THERM_C*pow(log(THERM_RESIST), 3))) - 273.15;
+  return valO2Therm;
 }
 
-void Sensors::getO2Comp(){
+float Sensors::getO2Comp(){
 	valO2 = (analogRead(*pO2)/(204.6*38.45*0.000585));
-	THERM_RESIST = 24900*((V3_3/(analogRead(*pO2Therm)/204.6))-1);
-	valO2Therm = (1.0/(THERM_A + THERM_B*log(THERM_RESIST) + THERM_C*pow(log(THERM_RESIST), 3))) - 273.15;
-	valO2Comp = valO2 + C3*pow(valO2Therm, 3) + C2*pow(valO2Therm, 2) + C1*valO2Therm + C0;
+	float O2Therm = getO2Therm();
+	valO2Comp = valO2 + C3*pow(O2Therm, 3) + C2*pow(O2Therm, 2) + C1*O2Therm + C0;
+  return valO2Comp;
 }
 
-void Sensors::getHeaterCurrent(){
+float Sensors::getHeaterCurrent(){
 	valHeaterCurrent = analogRead(*pHeaterCurrent)/(204.6*200*0.01);
+  return valHeaterCurrent;
 }
 
--- Sensors.h
+++ Sensors.h
@@ -3,6 +3,7 @@
 
 #include "O2-Cycler.h"
 #include <math.h>
+#include <Adafruit_MAX31855.h>
 
 class Sensors {
 	static unsigned int *pP_Abs, *pP_Gauge, *pTherm, *pFlow, *pTFlow, *pO2, *pO2Therm, *pHeaterCurrent;
@@ -14,26 +15,27 @@ class Sensors {
 	const double C3 = -0.000004333;
 	const double C0 = -(C3*pow(20, 3) + C2*pow(20, 1) + C1*20);
 	float THERM_RESIST = 0.0;
+  Adafruit_MAX31855 thermocouple;
 	public:
 	float valP_Abs, valP_Gauge, valTherm, valFlow, valTFlow, valO2, valO2Therm, valO2Comp, valHeaterCurrent = 0;
 	Sensors (int);
-	void getP_Abs();
+	float getP_Abs();
 	void printP_Abs();
-	void getP_Gauge();
+	float getP_Gauge();
 	void printP_Gauge();
-	void getTherm();
+	float getTherm();
 	void printTherm();
-	void getFlow();
+	float getFlow();
 	void printFlow();
-	void getTFlow();
+	float getTFlow();
 	void printTFlow();
-	void getO2();
+	float getO2();
 	void printO2();
-	void getO2Therm();
+	float getO2Therm();
 	void printO2Therm();
-	void getO2Comp();			//It is not required to get O2 and O2Therm vals separately before executing this function: it has all the functionality of the aformentioned functions
+	float getO2Comp();			//It is not required to get O2 and O2Therm vals separately before executing this function: it has all the functionality of the aformentioned functions
 	void printO2Comp();
-	void getHeaterCurrent();
+	float getHeaterCurrent();
 	void printHeaterCurrent();
 };
 
